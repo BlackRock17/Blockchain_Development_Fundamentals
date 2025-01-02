cea12401c926bda9a78f06881df614854c0537d2
BlackRock17 <lyyubo@gmail.com>
2025-01-02 17:21:34 +0200

    Created Simple Voting contract

--- SimpleVoting/simple_voting.sol
+++ SimpleVoting/simple_voting.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+enum VotingOptions {
+    CandidateOne,
+    CandidateTwo
+}
+
+contract SimpleVoting {
+    uint256 public candidade1;
+    uint256 public candidade2;
+
+    function vote(VotingOptions candidate) external {
+        if(candidate == VotingOptions.CandidateOne) {
+            candidade1 += 1;
+        } else if(candidate == VotingOptions.CandidateTwo) {
+            candidade2++;
+        } 
+    }
+}
