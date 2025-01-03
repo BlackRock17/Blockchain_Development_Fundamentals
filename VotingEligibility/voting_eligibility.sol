1583def2346df6b1f2737d67701267ef27382b73
BlackRock17 <lyyubo@gmail.com>
2025-01-03 17:07:15 +0200

    Add contract '' Voting Eligibility''

--- VotingEligibility/voting_eligibility.sol
+++ VotingEligibility/voting_eligibility.sol
@@ -2,5 +2,13 @@
 pragma solidity 0.8.28;
 
 contract VotingEligibility {
-    
+    error TooYoungToVote(uint age);
+
+    function checkEligibility(uint age) public pure returns (bool) {
+        if(age < 18) {
+            revert TooYoungToVote(age);
+        }
+
+        return true;
+    }
 }
