41d0236d74d3dba4777ece6e9f4b8770352cae60
BlackRock17 <lyyubo@gmail.com>
2025-01-03 18:39:20 +0200

    add contract ''Bill Splitter''

--- BillSplitter/bill_splitter.sol
+++ BillSplitter/bill_splitter.sol
@@ -1,3 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+contract BillSplitter {
+   error InvalidAmount(uint256 amount);
+   error InvalidPeopleCount(uint256 people);
+   error CannotSplitEvenly(uint256 amount, uint256 people);
+   
+   function splitExpense(uint256 totalAmount, uint256 numPeople) public pure returns (uint256) {
+       if (totalAmount == 0) {
+           revert InvalidAmount(totalAmount);
+       }
+       if (numPeople == 0) {
+           revert InvalidPeopleCount(numPeople);
+       }
+       if (totalAmount % numPeople != 0) {
+           revert CannotSplitEvenly(totalAmount, numPeople);
+       }
+       
+       return totalAmount / numPeople;
+   }
+}
