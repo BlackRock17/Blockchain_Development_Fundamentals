7d13a61e9fff78de1070a1ee83ac878c9eb0eb54
BlackRock17 <lyyubo@gmail.com>
2025-01-03 16:18:02 +0200

    Add contract ''TemperatureConverter''

--- TemperatureConversion/temperature_conversion.sol
+++ TemperatureConversion/temperature_conversion.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+
+contract TemperatureConverter {
+    function toFahrenheit(int celsius) public pure returns (int) {
+        return (celsius * 9/5) + 32;
+    }
+
+    function toCelsius(int fahrenheit) public pure returns (int) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
