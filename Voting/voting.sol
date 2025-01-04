45819362eb475db6bd6184f8b25bb6639524c4a5
BlackRock17 <lyyubo@gmail.com>
2025-01-05 00:35:55 +0200

    add function ''Register Vote''

--- Voting/voting.sol
+++ Voting/voting.sol
@@ -2,5 +2,22 @@
 pragma solidity 0.8.28;
 
 contract Voting {
-    
+
+    struct Voter {
+        bool hasVoted;
+        uint256 choice;
+    }
+
+    mapping(address => Voter) public voters;
+
+    event VoteRegistered(address voter, uint256 choice);
+
+    function registerVote(uint256 _candidateId) public {
+        require(!voters[msg.sender].hasVoted, "You have already voted!");
+
+        voters[msg.sender] = Voter({
+            hasVoted: true,
+            choice: _candidateId
+        });
+    }
 }
