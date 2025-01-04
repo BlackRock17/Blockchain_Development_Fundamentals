91e604a1d77723854ed963513620bc50b1f1a746
BlackRock17 <lyyubo@gmail.com>
2025-01-04 23:39:17 +0200

    add function ''Store Message''

--- MessageBoard/message_board.sol
+++ MessageBoard/message_board.sol
@@ -3,4 +3,8 @@ pragma solidity 0.8.28;
 
 contract messageBoard {
     mapping(address => string) public messages;
+
+    function storeMessage(string memory _message) public {
+        messages[msg.sender] = _message;
+    }
 }
