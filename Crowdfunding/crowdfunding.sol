4bd7e94a90cf7887e4a04044c78601896e78596a
BlackRock17 <lyyubo@gmail.com>
2025-01-06 21:45:08 +0200

    created contract ''Crowdfunding''

--- Crowdfunding/crowdfunding.sol
+++ Crowdfunding/crowdfunding.sol
@@ -1,3 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+contract Crowdfunding {
+
+    struct {
+        uint256 goalAmound;
+        uint256 totalCollected;
+        uint256 endTime;
+        bool goalReached;
+        bool fundsClaimed;
+    }
+
+    Campaign public campaign;
+
+    mapping(address => uint256) public contributions;
+
+    
+}
