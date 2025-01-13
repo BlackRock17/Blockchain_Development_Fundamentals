2992c80e945937b848378059c7a87872db79f432
BlackRock17 <lyyubo@gmail.com>
2025-01-13 17:45:39 +0200

    add events

--- DigitalLibrary/digital_library.sol
+++ DigitalLibrary/digital_library.sol
@@ -18,4 +18,17 @@ contract DigitalLibrary {
         address primaryLibrarian;
         uint256 readCount;
     }
+
+    mapping(uint256 => EBook) public ebooks;
+
+    mapping(uint256 => mapping(address => bool)) public authorizedLibrarians;
+
+    uint256 private nextEbookID;
+
+    // Events
+    event EBookCreated(uint256 indexed ebookId, string title, address primaryLibrarian);
+    event LibrarianAdded(uint256 indexed ebookId, address librarian);
+    event ExpirationExtended(uint256 indexed ebookId, uint256 newExpirationDate);
+    event StatusChanged(uint256 indexed ebookId, BookStatus newStatus);
+
 }
