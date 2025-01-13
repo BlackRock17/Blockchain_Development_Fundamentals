573471eb26eaf6eca91a93237839ccb24323df6a
BlackRock17 <lyyubo@gmail.com>
2025-01-13 17:40:42 +0200

    add contract ''DigitalLibrary''

--- DigitalLibrary/digital_library.sol
+++ DigitalLibrary/digital_library.sol
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+
+contract DigitalLibrary {
+
+    enum BookStatus {
+        Active,
+        Outdated,
+        Archived
+    }
+
+    struct EBook {
+        string title;
+        string author;
+        uint256 publicationDate;
+        uint256 expirationDate;
+        BookStatus status;
+        address primaryLibrarian;
+        uint256 readCount;
+    }
+}
