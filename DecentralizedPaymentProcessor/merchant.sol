c92b5dc549003cd134968a94a687c31aa21dab0b
BlackRock17 <lyyubo@gmail.com>
2025-01-14 16:36:20 +0200

    add function "refundPayment" with bonus logic

--- DecentralizedPaymentProcessor/merchant.sol
+++ DecentralizedPaymentProcessor/merchant.sol
@@ -11,5 +11,25 @@ contract Merchant is PaymentProcessor {
 
     event LoyaltyBonusApplied(address indexed customer, uint256 bonusAmount);
 
-    
+    function refundPayment(uint256 amount) public override {
+
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        require(amount > 0, "Refund amount must be greater than 0");
+
+        uint256 totalRefund = amount;
+
+        if (balances[msg.sender] >= LOYALTY_THRESHOLD) {
+
+            uint256 bonus = (amount * LOYALTY_BONUS_PERCENT) / 100;
+            totalRefund += bonus;
+
+            require(balances[msg.sender] >= totalRefund, "Insufficient balance for bonus");
+            emit LoyaltyBonusApplied(msg.sender, bonus);
+        }
+
+        balances[msg.sender] -= totalRefund;
+        payable(msg.sender).transfer(totalRefund);
+
+        emit RefundProcessed(msg.sender, totalRefund); 
+    }
 }
