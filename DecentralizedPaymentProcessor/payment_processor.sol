c424d8e6685e1d5c02293cf548d5bd0c65bd35bd
BlackRock17 <lyyubo@gmail.com>
2025-01-14 15:58:09 +0200

    add contract ''PaymentProcessor''

--- DecentralizedPaymentProcessor/payment_processor.sol
+++ DecentralizedPaymentProcessor/payment_processor.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+
+contract PaymentProcessor {
+
+    mapping(address => uint256) public balances;
+
+    event PaymentReceived(address indexed customer, uint256 amount);
+    event RefundProcessed(address indexed customer, uint256 amount);
+
+    
+}
