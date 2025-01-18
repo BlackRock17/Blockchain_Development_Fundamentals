542b15c9cccb3f43b13069b7ec4b1bfd6dccf67b
BlackRock17 <lyyubo@gmail.com>
2025-01-19 01:50:24 +0200

    Updated contract ''Asset''

--- AssetFactory/asset_factory.sol
+++ AssetFactory/asset_factory.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.28;
 
 contract Asset {
+    string public symbol;
+    string public name;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balances;
     
 }
