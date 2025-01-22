4a93b21217d6a02a62e8605ad7cb5a64f1cae7ca
BlackRock17 <lyyubo@gmail.com>
2025-01-22 16:25:57 +0200

    created contract ''AssetFactory''

--- AssetFactory/asset_factory.sol
+++ AssetFactory/asset_factory.sol
@@ -1,3 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+import "./asset.sol"
+
+contract AssetFactory {
+    mapping(string => address) public assets;
+
+    event AssetCreated(string symbol, address AssetAddress);
+    event TransferExecuted(string symbol, address from, address to, uint256 amount);
+
+    
+}
