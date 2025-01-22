5ab1271fcba24b91600e5ea88ef9b7dc153f1357
BlackRock17 <lyyubo@gmail.com>
2025-01-22 16:32:58 +0200

    add function "createAsset"

--- AssetFactory/asset_factory.sol
+++ AssetFactory/asset_factory.sol
@@ -9,5 +9,17 @@ contract AssetFactory {
     event AssetCreated(string symbol, address AssetAddress);
     event TransferExecuted(string symbol, address from, address to, uint256 amount);
 
-    
+    function createAsset(
+        string memory _symbol,
+        string memory _name,
+        uint256 _initialSupply
+    ) external returns (address) {
+        require(assets[_symbol] == address(0), "Asset with this symbol already exist");
+
+        Asset newAsset = new Asset(_symbol, _name, _initialSupply);
+        assets[_symbol] = address(newAsset);
+
+        emit AssetCreated(_symbol, address(newAsset));
+        return address(newAsset);
+    }
 }
