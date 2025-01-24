2624b5db9504bbda37d3913b4a10d80f140d3d8a
BlackRock17 <lyyubo@gmail.com>
2025-01-24 18:09:58 +0200

    updated function ''mintTicket''

--- NFT-Based-Ticketing-System/nftTicketingSystem.sol
+++ NFT-Based-Ticketing-System/nftTicketingSystem.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract EventTicketNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _toketIds;
+    Counters.Counter private _tokenIds;
 
     struct TicketMetadata {
         string eventName;
@@ -16,14 +16,14 @@ contract EventTicketNFT is ERC721, Ownable {
         bool isValid;
     }
 
-    mapping(uint256 => TicketMetadata) private _TicketMetadata;
+    mapping(uint256 => TicketMetadata) private _ticketMetadata;
     mapping(address => bool) private _aprrovedTransferers;
 
     event TicketMinted(address indexed to, uint256 indexed tokenId, string eventName, string seatNumber);
     event TicketTransferred(address indexed from, address indexed to, uint256 indexed tokenId);
     event TransfererStatusUpdated(address indexed transferer, bool status);
 
-    constructor() ERC721("EventTicketNFT", "ETN") {}
+    constructor() ERC721("EventTicketNFT", "ETN") Ownable(msg.sender) {}
 
     function mintTicket(
         address to,
@@ -31,6 +31,18 @@ contract EventTicketNFT is ERC721, Ownable {
         uint256 eventDate,
         string memory seatNumber
     ) public onlyOwner returns (uint256) {
-        
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _safeMint(to, newTokenId);
+        _ticketMetadata[newTokenId] = TicketMetadata(
+            eventName,
+            eventDate,
+            seatNumber,
+            true
+        );
+
+        emit TicketMinted(to, newTokenId, eventName, seatNumber);
+        return newTokenId;
     }
 }
