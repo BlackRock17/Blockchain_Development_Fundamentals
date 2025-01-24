1900140cae4a5525d028773579b9a5bb6f130730
BlackRock17 <lyyubo@gmail.com>
2025-01-24 17:50:56 +0200

    add contract ''EventTicketNFT''

--- NFT-Based-Ticketing-System/nftTicketingSystem.sol
+++ NFT-Based-Ticketing-System/nftTicketingSystem.sol
@@ -4,3 +4,20 @@ pragma solidity 0.8.28;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract EventTicketNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _toketIds;
+
+    struct TicketMetadata {
+        string eventName;
+        uint256 eventDate;
+        string seatNumber;
+        bool isValid;
+    }
+
+    mapping(uint256 => TicketMetadata) private _TicketMetadata;
+    mapping(address => bool) private _aprrovedTransferers;
+
+    
+}
