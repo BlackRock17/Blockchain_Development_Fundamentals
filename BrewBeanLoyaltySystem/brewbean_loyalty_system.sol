450a14c0b0bf0ec7f2df03be4dce05092983b493
BlackRock17 <lyyubo@gmail.com>
2025-01-26 14:59:11 +0200

    add interface ''ILoyaltyPoints"

--- BrewBeanLoyaltySystem/brewbean_loyalty_system.sol
+++ BrewBeanLoyaltySystem/brewbean_loyalty_system.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
+
+interface ILoyaltyPoints {
+    
+    function rewardPoints(address customer, uint256 amount) external returns (bool);
+
+    function redeemPoints(uint256 amount) external returns (bool);
+
+    event Rewarded(address indexed customer, uint256 amount);
+
+    event Redeemed(address indexed customer, uint256 amount);
+}
