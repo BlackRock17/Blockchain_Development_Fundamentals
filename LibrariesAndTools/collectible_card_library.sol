76461aa8addfddded9d176a3092b5d0ed1579e5b
BlackRock17 <lyyubo@gmail.com>
2025-01-30 18:32:32 +0200

    add function "addCard"

--- LibrariesAndTools/collectible_card_library.sol
+++ LibrariesAndTools/collectible_card_library.sol
@@ -5,5 +5,26 @@ import "./CardLibrary.sol";
 
 contract CollectibleCardLibrary {
     using CardLibrary for Card[];
-    
+
+    mapping(address => Card[]) private collections;
+
+    event CardAdded(address indexed owner, uint256 id, string name);
+
+    function addCard(uint256 id, string memory name) public {
+       // Check if the card already exists in the user's collection
+        require(!collections[msg.sender].exists(id), "Card with this ID already exists in your collection");
+        
+        // Create a new map
+        Card memory newCard = Card({
+            id: id,
+            name: name,
+            owner: msg.sender
+        });
+        
+        // Add the card to the user's collection
+        collections[msg.sender].push(newCard);
+        
+        // Emit an event for the added card
+        emit CardAdded(msg.sender, id, name);
+    }
 }
