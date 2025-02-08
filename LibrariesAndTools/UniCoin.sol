55778bf86d8c7cf22edfc6464e3276fa2b127196
BlackRock17 <lyyubo@gmail.com>
2025-02-08 18:23:04 +0200

    Created contract "UniCoin"

--- LibrariesAndTools/UniCoin.sol
+++ LibrariesAndTools/UniCoin.sol
@@ -3,3 +3,8 @@ pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract UniCoin is ERC20, Ownable {
+    constructor() ERC20("UniCoin", "UNI") {}
+    
+}
