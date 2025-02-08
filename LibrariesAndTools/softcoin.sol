841f84acdb3b6d2a8651b1cde4dc1e65785f4343
BlackRock17 <lyyubo@gmail.com>
2025-02-08 18:19:09 +0200

    add function "mint"

--- LibrariesAndTools/softcoin.sol
+++ LibrariesAndTools/softcoin.sol
@@ -6,4 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract SoftCoin is ERC20 {
     constructor() ERC20("SoftCoin", "SFT") {}
 
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
+
 }
