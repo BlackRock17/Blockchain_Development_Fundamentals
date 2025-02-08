d22b7587bebdad85c6c51511fbd0e3be96cdc098
BlackRock17 <lyyubo@gmail.com>
2025-02-08 18:23:45 +0200

    add function "mint"

--- LibrariesAndTools/UniCoin.sol
+++ LibrariesAndTools/UniCoin.sol
@@ -7,4 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract UniCoin is ERC20, Ownable {
     constructor() ERC20("UniCoin", "UNI") {}
     
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
