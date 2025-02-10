ab69e8edfbe02e189ec6b86e1c369f4d7c3c1af1
BlackRock17 <lyyubo@gmail.com>
2025-02-10 18:24:39 +0200

    Created contract "TokenExchange"

--- LibrariesAndTools/TokenExchange.sol
+++ LibrariesAndTools/TokenExchange.sol
@@ -5,3 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./UniCoin.sol";
 import "./SoftCoin.sol";
 
+contract TokenExchange {
+    SoftCoin public softCoin;
+    UniCoin public uniCoin;
+
+    
+
+}
